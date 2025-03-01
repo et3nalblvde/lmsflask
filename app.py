71b0de713c47b53ee661dadc806251d551fcf6b5
et3nalblvde <arokii2001@gmail.com>
2025-03-01 11:21:07 +0300

    Пейзажи Марса

--- app.py
+++ app.py
@@ -1,30 +1,13 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
-from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 Bootstrap(app)
 
-UPLOAD_FOLDER = 'static/img/'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file:
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file', filename=filename))
-    return render_template('upload.html')
-
-
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return render_template('uploaded.html', filename=filename)
-
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
