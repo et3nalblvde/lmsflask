4cf830e5a53dff87d8747006ae9aaaf39cc8ac95
et3nalblvde <arokii2001@gmail.com>
2025-03-01 11:22:53 +0300

    Реклама с картинкой

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-import os
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 Bootstrap(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/promotion_image')
+def promotion_image():
+    return render_template('promotion_image.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
