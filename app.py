1de3d8354d753c687ec9b56cd0808f5e7cdfa0c7
et3nalblvde <arokii2001@gmail.com>
2025-03-01 11:17:50 +0300

    Загрузка файла

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 Bootstrap(app)
 
-@app.route('/results/<nickname>/<int:level>/<float:rating>')
-def results(nickname, level, rating):
-    return render_template('results.html', nickname=nickname, level=level, rating=rating)
+UPLOAD_FOLDER = 'static/img/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return render_template('upload.html')
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return render_template('uploaded.html', filename=filename)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
