1c935d144a34782eba695d43f9218e02a0416b59
et3nalblvde <arokii2001@gmail.com>
2025-03-01 11:07:07 +0300

    Варианты выбора

--- app.py
+++ app.py
@@ -1,30 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
+@app.route('/choice/<planet_name>')
+def choice(planet_name):
+    return render_template('choice.html', planet_name=planet_name)
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-        surname = request.form["surname"]
-        name = request.form["name"]
-        email = request.form["email"]
-        education = request.form["education"]
-        profession = request.form["profession"]
-        gender = request.form["gender"]
-        motivation = request.form["motivation"]
-        stay = request.form["stay"]
-
-        return f"<h2>Спасибо за вашу заявку, {name} {surname}!</h2>"
-
-    professions = [
-        "Инженер-исследователь", "Пилот", "Строитель", "Экзобиолог", "Врач", "Инженер по терраформированию",
-        "Климатолог", "Специалист по радиационной защите", "Астрогеолог", "Гляциолог", "Инженер жизнеобеспечения",
-        "Метеоролог", "Оператор марсохода", "Киберинженер", "Штурман", "Пилот дронов"
-    ]
-
-    return render_template("form.html", professions=professions)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
