99313299a51478eca2aac8925af3892b457a4046
et3nalblvde <arokii2001@gmail.com>
2025-03-07 13:21:40 +0300

    Список профессий

--- app.py
+++ app.py
@@ -2,14 +2,28 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index/<title>')
-def index(title="Заготовка"):
-    return render_template('base.html', title=title)
+professions = [
+    "инженер-исследователь",
+    "пилот",
+    "строитель",
+    "экзобиолог",
+    "врач",
+    "инженер по терраформированию",
+    "климатолог",
+    "специалист по радиационной защите",
+    "астрогеолог",
+    "гляциолог",
+    "инженер жизнеобеспечения",
+    "метеоролог",
+    "оператор марсохода",
+    "киберинженер",
+    "штурман",
+    "пилот дронов"
+]
 
-@app.route('/training/<prof>')
-def training(prof):
-    return render_template('training.html', prof=prof)
+@app.route('/list_prof/<list_type>')
+def list_prof(list_type):
+    return render_template('list_prof.html', list_type=list_type, professions=professions)
 
 if __name__ == '__main__':
     app.run(debug=True)
