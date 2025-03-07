ac3ba42f26a8fff1964d6e5aaf8be29c592ccc52
et3nalblvde <arokii2001@gmail.com>
2025-03-07 13:25:16 +0300

    Автоматический ответ

--- app.py
+++ app.py
@@ -1,29 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-professions = [
-    "инженер-исследователь",
-    "пилот",
-    "строитель",
-    "экзобиолог",
-    "врач",
-    "инженер по терраформированию",
-    "климатолог",
-    "специалист по радиационной защите",
-    "астрогеолог",
-    "гляциолог",
-    "инженер жизнеобеспечения",
-    "метеоролог",
-    "оператор марсохода",
-    "киберинженер",
-    "штурман",
-    "пилот дронов"
-]
+@app.route('/')
+@app.route('/index/<title>')
+def index(title="Заготовка"):
+    return render_template('base.html', title=title)
 
-@app.route('/list_prof/<list_type>')
-def list_prof(list_type):
-    return render_template('list_prof.html', list_type=list_type, professions=professions)
+@app.route('/answer')
+@app.route('/auto_answer')
+def auto_answer():
+    user_data = {
+        "title": "Анкета участника миссии",
+        "surname": request.args.get('surname', 'Иванов'),
+        "name": request.args.get('name', 'Иван'),
+        "education": request.args.get('education', 'высшее'),
+        "profession": request.args.get('profession', 'инженер'),
+        "sex": request.args.get('sex', 'мужской'),
+        "motivation": request.args.get('motivation', 'Исследовать новые миры!'),
+        "ready": request.args.get('ready', 'True')
+    }
+    return render_template('auto_answer.html', **user_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
